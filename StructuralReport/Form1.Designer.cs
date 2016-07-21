893470c64c490a8a9f559ee72cede96ecc0e9be5
ChaosL <lizc47@gmail.com>
2016-07-21 16:50:22 +0800

    添加了listbox

--- StructuralReport/Form1.Designer.cs
+++ StructuralReport/Form1.Designer.cs
@@ -1,11 +1,12 @@
 ﻿namespace StructuralReport
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 必需的设计器变量。
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private ReportXML reportLists;
 
         /// <summary>
         /// 清理所有正在使用的资源。
@@ -28,12 +29,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.templatelistBox = new System.Windows.Forms.ListBox();
+            this.TemplateListLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // templatelistBox
+            // 
+            this.templatelistBox.FormattingEnabled = true;
+            this.templatelistBox.ItemHeight = 16;
+            //this.templatelistBox.Items.AddRange(new object[] {
+            //"CT Brain",
+            //"MR Ankle"});
+            this.templatelistBox.Location = new System.Drawing.Point(0, 49);
+            this.templatelistBox.Margin = new System.Windows.Forms.Padding(4);
+            this.templatelistBox.Name = "templatelistBox";
+            this.templatelistBox.Size = new System.Drawing.Size(236, 660);
+            this.templatelistBox.TabIndex = 0;
+            // 
+            // TemplateListLabel
+            // 
+            this.TemplateListLabel.AutoSize = true;
+            this.TemplateListLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TemplateListLabel.Enabled = false;
+            this.TemplateListLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.TemplateListLabel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TemplateListLabel.Location = new System.Drawing.Point(4, 16);
+            this.TemplateListLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TemplateListLabel.Name = "TemplateListLabel";
+            this.TemplateListLabel.Size = new System.Drawing.Size(74, 18);
+            this.TemplateListLabel.TabIndex = 1;
+            this.TemplateListLabel.Text = "模板列表";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(276, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(935, 392);
+            this.panel1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1255, 720);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TemplateListLabel);
+            this.Controls.Add(this.templatelistBox);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "结构化报告";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox templatelistBox;
+        private System.Windows.Forms.Label TemplateListLabel;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
---
+++ StructuralReport/ReportXML.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Xml;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace StructuralReport
+{ 
+        public class ReportXML
+        {
+            //private:
+            string fileName;
+            XmlDocument reportxml = new XmlDocument();//初始化一个xml实例
+            ArrayList reportlists = new ArrayList();
+        
+            
+            public ReportXML()
+	        {
+                fileName = "C:\\Users\\chaos\\OneDrive\\结构化报告\\SR.XML";
+            }
+            public ReportXML(string filename)
+            {
+                fileName = filename;
+            }
+
+            public Boolean Initialize()
+            {
+                try
+                {
+                    reportxml.Load(fileName);//导入指定xml文件
+                    XmlNode root = reportxml.SelectSingleNode("/StructuralReports");//指定一个节点
+                    XmlNodeList reportlists = root.ChildNodes;//获取节点下所有直接子节点
+                                                              //XmlNodeList nodelist = xml.SelectNodes("/Root/News");//获取同名同级节点集合string id=node.Attributes["id"].Value;//获取指定节点的指定属性值
+                                                              //string content = node.InnerText;//获取指定节点中的文本
+                                                              //root.HasChildNodes;//判断该节点下是否有子节点
+
+                    foreach (XmlNode report in reportlists)
+                    {
+                        XmlElement reportname = (XmlElement)report;
+                        this.reportlists.Add(reportname.GetAttribute("name"));
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString(), "错误");
+
+                    throw;
+                }
+
+                return true;
+
+            }
+            
+            public ArrayList GetReportLists()
+            {
+                return this.reportlists;
+            }
+
+        }
+}
